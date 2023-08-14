4c37ec0e1d326ecd78209ad4de71b1b09e4b0d3b
akwasiaboagye <saboagy5@gmail.com>
2023-08-14 05:05:39 -1200

    updated

---
+++ build/precache-manifest.1da264b0b45035e7bf14c9268c4b9004.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff67b5f2aeffb675d15f",
+    "url": "/static/css/main.4666c164.chunk.css"
+  },
+  {
+    "revision": "ff67b5f2aeffb675d15f",
+    "url": "/static/js/main.ff67b5f2.chunk.js"
+  },
+  {
+    "revision": "04ab9e61943e27d2b995",
+    "url": "/static/js/1.04ab9e61.chunk.js"
+  },
+  {
+    "revision": "25a01c00bc0351b3c342",
+    "url": "/static/js/2.25a01c00.chunk.js"
+  },
+  {
+    "revision": "00dfd99c95d342aa8c1d",
+    "url": "/static/js/3.00dfd99c.chunk.js"
+  },
+  {
+    "revision": "341c9cacdb531697c135",
+    "url": "/static/js/4.341c9cac.chunk.js"
+  },
+  {
+    "revision": "c22b4e6a5d0eb756d451",
+    "url": "/static/js/5.c22b4e6a.chunk.js"
+  },
+  {
+    "revision": "f289e069d836f1a165e5",
+    "url": "/static/js/6.f289e069.chunk.js"
+  },
+  {
+    "revision": "73d4b9afd0f540fd6e2b",
+    "url": "/static/js/7.73d4b9af.chunk.js"
+  },
+  {
+    "revision": "3eebebdf75915976069b",
+    "url": "/static/js/runtime~main.3eebebdf.js"
+  },
+  {
+    "revision": "b36d104fca159f086b7dcfebba88e9fc",
+    "url": "/index.html"
+  }
+];
