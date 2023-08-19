e84eb5edb4b68ba15be8a2c292943ffc2774afe2
akwasiaboagye <saboagy5@gmail.com>
2023-08-18 22:58:09 -1200

    updated

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const TopBar = () => {
+
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const MobileMenu = () => {
+    setMobileOpen(prevState => !prevState)
+  };
+ 
   return (
     <div>
        <header id="header" class="header d-flex align-items-center">
@@ -11,9 +18,21 @@ const TopBar = () => {
               Maskaf Construction<span>.</span>
             </h1>
           </a>
-
-          <i class="mobile-nav-toggle mobile-nav-show bi bi-list" id="navbar"></i>
-          <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+          <button className='btn btn-default' onClick={MobileMenu}>
+            <i class="mobile-nav-toggle mobile-nav-show bi bi-list" ></i>
+            <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+          </button>
+         {mobileOpen && (
+          <nav id="navbar" class="navbar">
+          <ul>
+            <li><a href="/mainview" class="active">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/service">Services</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+         )}
           <nav id="navbar" class="navbar">
             <ul>
               <li><a href="/mainview" class="active">Home</a></li>
